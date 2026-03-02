b6fbb8cb4edb3fff11cff7b10523c99b1cd907a3
Eleni <elenimariex@gmail.com>
2026-03-02 15:42:43 +0000

    Adds formatting

--- gcp-project/platform-playground.tf
+++ gcp-project/platform-playground.tf
@@ -1,8 +1,8 @@
 terraform {
-    backend "gcs" {
-      bucket = "terraform-state-bucket-elenig"
-      prefix = "gcp-project/platform_playground"
-    }
+  backend "gcs" {
+    bucket = "terraform-state-bucket-elenig"
+    prefix = "gcp-project/platform_playground"
+  }
 
 }
 
